56ad2556bfa98a512adc69db5942a9ea7c6fb707
unknown <shezansems@gmail.com>
2021-04-07 21:26:14 -0400

    add navigation bar

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import ColorBox from "./ColorBox";
 import "../styles/Palette.less";
-import { Slider, Switch } from "antd";
-//import "antd/dist/antd.css";
+import Navbar from "./Navbar";
 
 export default function Palette(props) {
   //set level
   const [level, setLevel] = useState(props.level);
-  function handleSlider(value) {
-    setLevel(value);
-  }
+
 
   //import ColorBoxes
   const colorBoxes = props.palette.colors[level].map((color) => {
@@ -18,17 +15,7 @@ export default function Palette(props) {
 
   return (
     <div className="Palette">
-      <div className="slider">
-        <Slider
-          defaultValue={props.level}
-          step={100}
-          max={900}
-          min={100}
-          onChange={handleSlider}
-        />
-      </div>
-
-      {/* {Navbar goes here} */}
+      <Navbar level={level} setLevel={setLevel} />
       <div className="Palette-colors"> {colorBoxes} </div>
       {/* {footer} */}
     </div>
