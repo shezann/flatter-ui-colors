72f7673f16f4646f85c037b7788e1d7942bf3c82
shezann <s23mahmu@uwaterloo.ca>
2021-04-17 21:18:31 -0400

    style draggable color box

--- src/components/DraggableColorBox.js
+++ src/components/DraggableColorBox.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "../styles/DraggableColorBox.less";
+import { DeleteFilled } from "@ant-design/icons";
 
 export default function DraggableColorBox(props) {
   const { color } = props;
 
   return (
     <div className="main" style={{ backgroundColor: color.color }}>
-      {color.name}
+      <div className="content">
+        <span>{color.name}</span>
+        <span>
+          <DeleteFilled className="delete-icon" />
+        </span>
+      </div>
     </div>
   );
 }
